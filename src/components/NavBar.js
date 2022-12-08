8413826d02025d4987155112134ba9924ce4fb70
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-12-08 16:12:58 -0600

    updating link refs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 // import F1Logo from './assets/F1LOGO.png';
-import F1Logo2 from './assets/f1_logo.svg';
+import F1Logo2 from './assets/f1_logo.svg'
 
 export const NavBar = () => {
-    return (
-        <>
-            <Navbar className='color-nav rounded navbar font-face-f1r' variant='dark' expand="sm">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt="formula 1"
-                            src={F1Logo2}
-                            width="100"
-                            height="25"
-                            className=""
-                        />{' '}
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/scorelist">Score List</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar
+        className="color-nav rounded navbar font-face-f1r"
+        variant="dark"
+        expand="sm"
+      >
+        <Container>
+          <Navbar.Brand href="week16-final-project/">
+            <img
+              alt="formula 1"
+              src={F1Logo2}
+              width="100"
+              height="25"
+              className=""
+            />{' '}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="week16-final-project/">Home</Nav.Link>
+              <Nav.Link href="week16-final-project/scorelist">
+                Score List
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
