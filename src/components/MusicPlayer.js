c1cf9f546667d58e5fd07f6ddb96a1379ec9a8e7
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-19 10:52:10 -0600

    placeholder code

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import BackgroundMusic from './assets/F1.mp3';
-import ReactAudioPlayer from 'react-audio-player';
+import BackgroundMusic from './assets/F1.mp3'
+import ReactAudioPlayer from 'react-audio-player'
 
 const MusicPlayer = () => {
-    
-    return (
-        <>
-            <ReactAudioPlayer src={BackgroundMusic} autoPlay controls/>
-        </>
-    )
+  //https://stackoverflow.com/questions/37949895/stop-audio-on-route-change-in-react
+  // // the audio variable needs to be stored in a ref in order to access it across renders
+  // let audio = useRef();
+  // // start the audio (using the .current property of the ref we just created) when the component mounts using the useEffect hook
+  // useEffect(() => {
+  //     audio.current = new Audio(waves)
+  //     audio.current.play()
+  // }, [])
+  // // Stop the audio when the component unmounts
+  // // (not exactly what you asked re React Router, but similar idea)
+  // useEffect(() => {
+  //     return () => {
+  //         audio.current.pause()
+  //         console.log("in cleanup")
+  //     }
+  // }, [])
+
+  return (
+    <>
+      <ReactAudioPlayer src={BackgroundMusic} autoPlay controls />
+    </>
+  )
 }
 
-export default MusicPlayer;
+export default MusicPlayer
 
 // https://www.npmjs.com/package/react-audio-player
